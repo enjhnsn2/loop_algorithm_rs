fe07dc226388692fbebc218334f9bc325e945c18
danielbonkowsky <danielbonkowsky@gmail.com>
2026-06-22 13:51:09 -0700

    std::fmt -> core::fmt

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -181,8 +181,8 @@ pub enum AlgorithmError {
     FutureBasalNotAllowed,
 }
 
-impl std::fmt::Display for AlgorithmError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl core::fmt::Display for AlgorithmError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{self:?}")
     }
 }
